8803cb1c45b1404e02e328f69e7832e64a0de40e
MichelFloyd <michel.floyd@cloaklabs.com>
2020-08-13 19:23:26 -0700

    try pickle instead of json.dumps

--- ingest.py
+++ ingest.py
@@ -9,6 +9,7 @@ import numpy
 import os
 import time
 import tqdm
+import pickle
 
 from detectron2.config import get_cfg
 from detectron2.data.detection_utils import read_image
@@ -85,7 +86,6 @@ def get_parser():
     )
     return parser
 
-
 if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
     args = get_parser().parse_args()
@@ -94,44 +94,9 @@ if __name__ == "__main__":
     logger.info("Arguments: " + str(args))
 
     cfg = setup_cfg(args)
-
     demo = VisualizationDemo(cfg)
 
-    # THIS WILL BE REMOVED - TODO
-    if args.input:
-        if len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
-            start_time = time.time()
-            predictions, visualized_output = demo.run_on_image(img)
-            logger.info(
-                "{}: {} in {:.2f}s".format(
-                    path,
-                    "detected {} instances".format(len(predictions["instances"]))
-                    if "instances" in predictions
-                    else "finished",
-                    time.time() - start_time,
-                )
-            )
-
-            if args.output:
-                if os.path.isdir(args.output):
-                    assert os.path.isdir(args.output), args.output
-                    out_filename = os.path.join(args.output, os.path.basename(path))
-                else:
-                    assert len(args.input) == 1, "Please specify a directory with args.output"
-                    out_filename = args.output
-                visualized_output.save(out_filename)
-            else:
-                assert args.output, "Please specify a directory for output"
-                cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-                cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-                if cv2.waitKey(0) == 27:
-                    break  # esc to quit
-    elif args.video_input:
+    if args.video_input:
         video = cv2.VideoCapture(args.video_input)
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -143,9 +108,7 @@ if __name__ == "__main__":
             if os.path.isdir(args.output):
                 output_fname = os.path.join(args.output, basename)
                 output_fname = os.path.splitext(output_fname)[0] + ".mkv"
-            # else:
-            #     output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
+            #assert not os.path.isfile(output_fname), output_fname
             output_file = cv2.VideoWriter(
                 filename=output_fname,
                 # some installation of opencv may not support x264 (due to its license),
@@ -161,7 +124,6 @@ if __name__ == "__main__":
         segments_data['categories'] = {}
         segments_data['annotations'] = []
         for prediction_thing, vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            breakpoint()
             # if args.output:
             output_file.write(vis_frame)
             segments_data['annotations'].append(prediction_thing)
@@ -173,7 +135,5 @@ if __name__ == "__main__":
         video.release()
         if args.output:
             with open(output_fname+".json", 'w') as segments_file:
-                json.dump(segments_data, segments_file, indent=2, cls=NumpyArrayEncoder)
+                pickle.dump(segments_data, segments_file, indent=2, cls=NumpyArrayEncoder)
             output_file.release()
-        else:
-            cv2.destroyAllWindows()
